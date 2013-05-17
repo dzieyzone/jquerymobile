8cb7f742744f900637cd1945d321c24ee6f4293d
joelmarkmagundayao <joel@ilovehongkong.hk>
2013-05-17 11:28:11 +0800

    add contact us page

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {	
+  $("#contact #contact-form .submit").click(function() {
+    var name = $(".name").val();
+	var mail = $(".mail").val();
+	var message = $(".message").val();
+	
+	$.ajax({
+	  type: 'POST', // defaults to 'GET'
+	  url: "http://localhost/jquerymobile/contact.php?name="+name+"&mail="+mail+"&message="+message,
+	  data: {name : ""},
+	  dataType: 'html', //'json', 'xml', 'html', or 'text'
+	  success: function(response) { 
+	    $(".messages").html(response);  
+	    if(response == "Message has been submitted."){
+	      $(".name").val("");
+	      $(".mail").val("");
+	      $(".message").val("");
+	    }
+	  },
+	  error: function(xhr, type) {alert("Error ajax call!"); }
+	});
+	
+  });	
+});
